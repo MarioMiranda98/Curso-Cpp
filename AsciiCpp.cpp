98d81f786c31ca921af579ffef4f99d7400963b0
MarioMiranda98 <farolin95@msn.com>
2019-12-27 19:43:16 -0600

    Ascii funcional

---
+++ AsciiCpp.cpp
@@ -0,0 +1,360 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "\0 No: 0" << endl; 
+	cout << "\1 No: 1" << endl; 
+	cout << "\2 No: 2" << endl; 
+	cout << "\3 No: 3" << endl; 
+	cout << "\4 No: 4" << endl; 
+	cout << "\5 No: 5" << endl; 
+	cout << "\6 No: 6" << endl; 
+	cout << "\7 No: 7" << endl; 
+	cout << "\10 No: 10" << endl; 
+	cout << "\11 No: 11" << endl; 
+	cout << "\12 No: 12" << endl; 
+	cout << "\13 No: 13" << endl; 
+	cout << "\14 No: 14" << endl; 
+	cout << "\15 No: 15" << endl; 
+	cout << "\16 No: 16" << endl; 
+	cout << "\17 No: 17" << endl; 
+	cout << "\20 No: 20" << endl; 
+	cout << "\21 No: 21" << endl; 
+	cout << "\22 No: 22" << endl; 
+	cout << "\23 No: 23" << endl; 
+	cout << "\24 No: 24" << endl; 
+	cout << "\25 No: 25" << endl; 
+	cout << "\26 No: 26" << endl; 
+	cout << "\27 No: 27" << endl; 
+	cout << "\28 No: 28" << endl; 
+	cout << "\29 No: 29" << endl; 
+	cout << "\30 No: 30" << endl; 
+	cout << "\31 No: 31" << endl; 
+	cout << "\32 No: 32" << endl; 
+	cout << "\33 No: 33" << endl; 
+	cout << "\34 No: 34" << endl; 
+	cout << "\35 No: 35" << endl; 
+	cout << "\36 No: 36" << endl; 
+	cout << "\37 No: 37" << endl; 
+	cout << "\40 No: 40" << endl; 
+	cout << "\41 No: 41" << endl; 
+	cout << "\42 No: 42" << endl; 
+	cout << "\43 No: 43" << endl; 
+	cout << "\44 No: 44" << endl; 
+	cout << "\45 No: 45" << endl; 
+	cout << "\46 No: 46" << endl; 
+	cout << "\47 No: 47" << endl; 
+	cout << "\50 No: 50" << endl; 
+	cout << "\51 No: 51" << endl; 
+	cout << "\52 No: 52" << endl; 
+	cout << "\53 No: 53" << endl; 
+	cout << "\54 No: 54" << endl; 
+	cout << "\55 No: 55" << endl; 
+	cout << "\56 No: 56" << endl; 
+	cout << "\57 No: 57" << endl; 
+	cout << "\60 No: 60" << endl; 
+	cout << "\61 No: 61" << endl; 
+	cout << "\62 No: 62" << endl; 
+	cout << "\63 No: 63" << endl; 
+	cout << "\64 No: 64" << endl; 
+	cout << "\65 No: 65" << endl; 
+	cout << "\66 No: 66" << endl; 
+	cout << "\67 No: 67" << endl; 
+	cout << "\70 No: 70" << endl; 
+	cout << "\71 No: 71" << endl; 
+	cout << "\72 No: 72" << endl; 
+	cout << "\73 No: 73" << endl; 
+	cout << "\74 No: 74" << endl; 
+	cout << "\75 No: 75" << endl; 
+	cout << "\76 No: 76" << endl; 
+	cout << "\77 No: 77" << endl; 
+	cout << "\100 No: 100" << endl; 
+	cout << "\101 No: 101" << endl; 
+	cout << "\102 No: 102" << endl; 
+	cout << "\103 No: 103" << endl; 
+	cout << "\104 No: 104" << endl; 
+	cout << "\105 No: 105" << endl; 
+	cout << "\106 No: 106" << endl; 
+	cout << "\107 No: 107" << endl; 
+	cout << "\110 No: 110" << endl; 
+	cout << "\111 No: 111" << endl; 
+	cout << "\112 No: 112" << endl; 
+	cout << "\113 No: 113" << endl; 
+	cout << "\114 No: 114" << endl; 
+	cout << "\115 No: 115" << endl; 
+	cout << "\116 No: 116" << endl; 
+	cout << "\117 No: 117" << endl; 
+	cout << "\120 No: 120" << endl; 
+	cout << "\121 No: 121" << endl; 
+	cout << "\122 No: 122" << endl; 
+	cout << "\123 No: 123" << endl; 
+	cout << "\124 No: 124" << endl; 
+	cout << "\125 No: 125" << endl; 
+	cout << "\126 No: 126" << endl; 
+	cout << "\127 No: 127" << endl; 
+	cout << "\130 No: 130" << endl; 
+	cout << "\131 No: 131" << endl; 
+	cout << "\132 No: 132" << endl; 
+	cout << "\133 No: 133" << endl; 
+	cout << "\134 No: 134" << endl; 
+	cout << "\135 No: 135" << endl; 
+	cout << "\136 No: 136" << endl; 
+	cout << "\137 No: 137" << endl; 
+	cout << "\140 No: 140" << endl; 
+	cout << "\141 No: 141" << endl; 
+	cout << "\142 No: 142" << endl; 
+	cout << "\143 No: 143" << endl; 
+	cout << "\144 No: 144" << endl; 
+	cout << "\145 No: 145" << endl; 
+	cout << "\146 No: 146" << endl; 
+	cout << "\147 No: 147" << endl; 
+	cout << "\150 No: 150" << endl; 
+	cout << "\151 No: 151" << endl; 
+	cout << "\152 No: 152" << endl; 
+	cout << "\153 No: 153" << endl; 
+	cout << "\154 No: 154" << endl; 
+	cout << "\155 No: 155" << endl; 
+	cout << "\156 No: 156" << endl; 
+	cout << "\157 No: 157" << endl; 
+	cout << "\160 No: 160" << endl; 
+	cout << "\161 No: 161" << endl; 
+	cout << "\162 No: 162" << endl; 
+	cout << "\163 No: 163" << endl; 
+	cout << "\164 No: 164" << endl; 
+	cout << "\165 No: 165" << endl; 
+	cout << "\166 No: 166" << endl; 
+	cout << "\167 No: 167" << endl; 
+	cout << "\170 No: 170" << endl; 
+	cout << "\171 No: 171" << endl; 
+	cout << "\172 No: 172" << endl; 
+	cout << "\173 No: 173" << endl; 
+	cout << "\174 No: 174" << endl; 
+	cout << "\175 No: 175" << endl; 
+	cout << "\176 No: 176" << endl; 
+	cout << "\177 No: 177" << endl; 
+	cout << "\200 No: 200" << endl; 
+	cout << "\201 No: 201" << endl; 
+	cout << "\202 No: 202" << endl; 
+	cout << "\203 No: 203" << endl; 
+	cout << "\204 No: 204" << endl; 
+	cout << "\205 No: 205" << endl; 
+	cout << "\206 No: 206" << endl; 
+	cout << "\207 No: 207" << endl; 
+	cout << "\210 No: 210" << endl; 
+	cout << "\211 No: 211" << endl; 
+	cout << "\212 No: 212" << endl; 
+	cout << "\213 No: 213" << endl; 
+	cout << "\214 No: 214" << endl; 
+	cout << "\215 No: 215" << endl; 
+	cout << "\216 No: 216" << endl; 
+	cout << "\217 No: 217" << endl; 
+	cout << "\220 No: 220" << endl; 
+	cout << "\221 No: 221" << endl; 
+	cout << "\222 No: 222" << endl; 
+	cout << "\223 No: 223" << endl; 
+	cout << "\224 No: 224" << endl; 
+	cout << "\225 No: 225" << endl; 
+	cout << "\226 No: 226" << endl; 
+	cout << "\227 No: 227" << endl; 
+	cout << "\230 No: 230" << endl; 
+	cout << "\231 No: 231" << endl; 
+	cout << "\232 No: 232" << endl; 
+	cout << "\233 No: 233" << endl; 
+	cout << "\234 No: 234" << endl; 
+	cout << "\235 No: 235" << endl; 
+	cout << "\236 No: 236" << endl; 
+	cout << "\237 No: 237" << endl; 
+	cout << "\240 No: 240" << endl; 
+	cout << "\241 No: 241" << endl; 
+	cout << "\242 No: 242" << endl; 
+	cout << "\243 No: 243" << endl; 
+	cout << "\244 No: 244" << endl; 
+	cout << "\245 No: 245" << endl; 
+	cout << "\246 No: 246" << endl; 
+	cout << "\247 No: 247" << endl; 
+	cout << "\250 No: 250" << endl; 
+	cout << "\251 No: 251" << endl; 
+	cout << "\252 No: 252" << endl; 
+	cout << "\253 No: 253" << endl; 
+	cout << "\254 No: 254" << endl; 
+	cout << "\255 No: 255" << endl; 
+}
+
+/*
+ No: 1
+ No: 2
+ No: 3
+ No: 4
+ No: 5
+ No: 6
+ No: 7
+ No: 10
+         No: 11
+
+ No: 12
+ No: 13
+ No: 14
+ No: 15
+ No: 16
+ No: 17
+ No: 20
+ No: 21
+ No: 22
+ No: 23
+ No: 24
+ No: 25
+ No: 26
+ No: 27
+8 No: 28
+9 No: 29
+ No: 30
+ No: 31
+ No: 32
+ No: 33
+ No: 34
+ No: 35
+ No: 36
+ No: 37
+  No: 40
+! No: 41
+" No: 42
+# No: 43
+$ No: 44
+% No: 45
+& No: 46
+' No: 47
+( No: 50
+) No: 51
+* No: 52
++ No: 53
+, No: 54
+- No: 55
+. No: 56
+/ No: 57
+0 No: 60
+1 No: 61
+2 No: 62
+3 No: 63
+4 No: 64
+5 No: 65
+6 No: 66
+7 No: 67
+8 No: 70
+9 No: 71
+: No: 72
+; No: 73
+< No: 74
+= No: 75
+> No: 76
+? No: 77
+@ No: 100
+A No: 101
+B No: 102
+C No: 103
+D No: 104
+E No: 105
+F No: 106
+G No: 107
+H No: 110
+I No: 111
+J No: 112
+K No: 113
+L No: 114
+M No: 115
+N No: 116
+O No: 117
+P No: 120
+Q No: 121
+R No: 122
+S No: 123
+T No: 124
+U No: 125
+V No: 126
+W No: 127
+X No: 130
+Y No: 131
+Z No: 132
+[ No: 133
+\ No: 134
+] No: 135
+^ No: 136
+_ No: 137
+` No: 140
+a No: 141
+b No: 142
+c No: 143
+d No: 144
+e No: 145
+f No: 146
+g No: 147
+h No: 150
+i No: 151
+j No: 152
+k No: 153
+l No: 154
+m No: 155
+n No: 156
+o No: 157
+p No: 160
+q No: 161
+r No: 162
+s No: 163
+t No: 164
+u No: 165
+v No: 166
+w No: 167
+x No: 170
+y No: 171
+z No: 172
+{ No: 173
+| No: 174
+} No: 175
+~ No: 176
+ No: 177
+Ç No: 200
+ü No: 201
+é No: 202
+â No: 203
+ä No: 204
+à No: 205
+å No: 206
+ç No: 207
+ê No: 210
+ë No: 211
+è No: 212
+ï No: 213
+î No: 214
+ì No: 215
+Ä No: 216
+Å No: 217
+É No: 220
+æ No: 221
+Æ No: 222
+ô No: 223
+ö No: 224
+ò No: 225
+û No: 226
+ù No: 227
+ÿ No: 230
+Ö No: 231
+Ü No: 232
+ø No: 233
+£ No: 234
+Ø No: 235
+× No: 236
+ƒ No: 237
+á No: 240
+í No: 241
+ó No: 242
+ú No: 243
+ñ No: 244
+Ñ No: 245
+ª No: 246
+º No: 247
+¿ No: 250
+® No: 251
+¬ No: 252
+½ No: 253
+¼ No: 254
+¡ No: 255
+*/
